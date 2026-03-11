eb58395e66f57cfc8bb7c37c6379966dfd2ab436
harmoos <harstn@tutanota.com>
2026-03-11 16:58:29 +0100

    Firsts lookup scripts

---
+++ local-lookup/browser_detector.py
@@ -0,0 +1,53 @@
+import shutil
+import os
+
+def get_installed_browsers():
+
+    browsers = {
+        "chrome": {
+            "exec": "google-chrome",
+            "config": os.path.expanduser("~/.config/google-chrome/"),
+            "type": "chromium"
+        },
+        "firefox": {
+            "exec": "firefox",
+            "config": os.path.expanduser("~/.mozilla/firefox/"),
+            "type": "firefox"
+        },
+        "brave": {
+            "exec": "brave-browser",
+            "config": os.path.expanduser("~/.config/BraveSoftware/"),
+            "type": "chromium"
+        },
+        "chromium": {
+            "exec": "chromium",
+            "config": os.path.expanduser("~/.config/chromium/"),
+            "type": "chromium"
+        },
+        "opera": {
+            "exec": "opera",
+            "config": os.path.expanduser("~/.config/opera/"),
+            "type": "chromium"
+        }
+    }
+
+    found = []
+    found_config = {}
+
+    for browser_name, data in browsers.items():
+        exec_found = shutil.which(data["exec"])
+        config_found = os.path.exists(data["config"])
+
+        if exec_found or config_found:
+            found.append(browser_name)
+        if config_found:
+            found_config[browser_name] = data
+
+    print("Detected Browsers:")
+    if len(found) > 0:
+        for b in found:
+            print(f"- {b}")
+    else:
+        print("No browsers detected.")
+
+    return found_config
---
+++ local-lookup/profile_hunter.py
@@ -0,0 +1,44 @@
+import os
+from browser_detector import get_installed_browsers
+
+def find_history_databases():
+    browsers = get_installed_browsers()
+    db_list = []
+
+    for name, data in browsers.items():
+        base_path = data["config"]
+        b_type = data["type"]
+
+        print(f"Looking for databases at: {name}")
+
+        if b_type == "chromium":
+            for item in os.listdir(base_path):
+                item_path = os.path.join(base_path, item)
+
+                if os.path.isdir(item_path) and (item == "Default" or item.startswith("Profile")):
+                    history_file = os.path.join(item_path, "History")
+                    if os.path.exists(history_file):
+                        print(f"  -> Profile found: {item}")
+                        print(f"     BD: {history_file}")
+                        db_list.append(history_file)
+
+        elif b_type == "firefox":
+            for item in os.listdir(base_path):
+                item_path = os.path.join(base_path, item)
+
+                if os.path.isdir(item_path):
+                    history_file = os.path.join(item_path, "places.sqlite")
+
+                    if os.path.exists(history_file):
+                        print(f"  -> Profile found: {item}")
+                        print(f"     BD: {history_file}")
+                        db_list.append(history_file)
+
+    print(f"Number of databases found: {len(db_list)}")
+
+    return db_list
+
+if __name__ == "__main__":
+    print("Starting")
+    databases = find_history_databases()
+    print("Done")
